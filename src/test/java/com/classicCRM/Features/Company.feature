883376d6cf456836c36ccf227d0947dae2bf7e47
shruti <ponksheshruti.gmail.com>
2023-05-19 20:02:17 +0530

    Changes related to maximizing window and deleting cookies added.

---
+++ src/test/java/com/classicCRM/Features/Company.feature
@@ -0,0 +1,15 @@
+
+@CompanyTest
+Feature: As a User I want to configure company details into system and use them in other sections 
+					like creating new contact and new Deal #only verifying on page that company is visible is handled in seperate scenarios
+
+  @DataCreationForCompany
+  Scenario: Add new Record for Company
+    Given Login to CRM Page
+    And Navigate to New Company Section
+    When I complete filling data form from excel data  
+    Then I click on save button
+    And then data should get saved successfully
+   
+
+
--- src/test/java/com/classicCRM/StepDef/CucumberHooks.java
+++ src/test/java/com/classicCRM/StepDef/CucumberHooks.java
@@ -1,13 +1,23 @@
 package com.classicCRM.StepDef;
 
+import java.time.Duration;
+
 import com.classicCRM.utils.Container;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 
 public class CucumberHooks {
 	Container contextClass;
 	public CucumberHooks(Container contextClass) {
 		this.contextClass=contextClass;
+	}
+	@Before
+	public void beforeSetup() {
+		contextClass.driver.manage().window().maximize();
+		contextClass.driver.manage().deleteAllCookies();
+		contextClass.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
 	}
 	@After
 	public void quitdriver() {
